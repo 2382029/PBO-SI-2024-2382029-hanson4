59bcbf454bb7ad3b25dea65c6711873b42e30924
Daryl <2382029@unai.edu>
2024-09-24 10:10:54 +0700

    handson4

---
+++ DenganMethod.java
@@ -0,0 +1,11 @@
+public class DenganMethod {
+    public static void main(String[] args) {
+        int gajiPokok = 5000000
+        int tunjanganTransport = 500000
+        int tunjanganMakan = 300000
+        int bonus = 1000000
+
+        int totalPenghasilan = hitungTotalPenghasilan(gajiPokok, tunjanganTransport, tunjanganMakan, Bonus);
+        int pajak = hitungPajak(totalPenghasilan)
+    }
+}
