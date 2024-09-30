02f26080f5f18ee1967e3319e6a966087b5a1ac7
Daryl <2382029@unai.edu>
2024-09-30 17:26:54 +0700

    handson4

--- ContohParameterMethod.java
+++ ContohParameterMethod.java
@@ -1,22 +1,15 @@
-import java.lang.foreign.PaddingLayout;
-import java.util.random.RandomGenerator;
-
-public class ContohParameterMethod {
-    // Method dengan satu parameter
-    public static int kaliDua(int angka) {
-        int hasil = angka * 2;
-        return hasil;
-    }
+public static int kaliDua ( int angka) {
+    int hasil = angka * 2;
+    return hasil;
 }
-    // Method dengan beberapa parameter
-    public static double hitungLuasSegitiga(double alas, double tinggi) {
-        return 0.5 * alas * tinggi;
-    }
+public  static  double hitungLuasSegitiga(double alas, double tinggi){
+    return 0.5 * alas * tinggi;
+}
 
-    public static void main(String[] args) {
-        int hasilKaliDua = ContohParameterMethod.kaliDua(5);
-        System.out.println("5 x 2 = " + hasilKaliDua);
+public static void main(String[] args) {
+    int hasilKaliDua = kaliDua( 5);
+    System.out.println("5 X 2 = * + hasilKaliDUa");
 
-        double luasSegitiga = hitungLuasSegitiga(4,6); // Memanggil method dengan argumen 4 dan 6
-        System.out.println("Luas segitiga: " + luasSegitiga);
-    }
+    double luasSegitiga = hitungLuasSegitiga(4, 6);
+    System.out.println("Luas Segitiga: " + luasSegitiga);
+}
--- DenganMethod.java
+++ DenganMethod.java
@@ -1,11 +1,18 @@
-public class DenganMethod {
+public class denganmethod {
     public static void main(String[] args) {
-        int gajiPokok = 5000000
-        int tunjanganTransport = 500000
-        int tunjanganMakan = 300000
-        int bonus = 1000000
+        int gajiPokok = 5000000;
+        int tunjanganTransport = 500000;
+        int tunjanganMakan = 300000;
+        int bonus = 1000000;
 
-        int totalPenghasilan = hitungTotalPenghasilan(gajiPokok, tunjanganTransport, tunjanganMakan, Bonus);
-        int pajak = hitungPajak(totalPenghasilan)
+        int totalPenghasilan = hitungTotalPenghasilan(gajiPokok, tunjanganTransport, tunjanganMakan, bonus);
+        int pajak = hitungPajak(totalPenghasilan);
+        int gajiBersih = hitungGajiBersih(totalPenghasilan, pajak);
+
+        tampilkanDetailGaji(gajiPokok, tunjanganTransport, tunjanganMakan, bonus, pajak, gajiBersih);
     }
-}
+
+    public static int hitungTotalPenghasilan(int gajiPokok, int tunjanganTransport,int tunjanganMakan, int bonus) {
+        return gajiPokok + tunjanganTransport + tunjanganMakan + bonus;
+    }
+
